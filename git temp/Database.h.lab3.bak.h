d6548b46e9d48ee93435129322005f912e412586
panzertime <panzer.time@gmail.com>
2015-12-01 09:07:08 -0700

    seem to have merged changes from lab4 back to lab3 code

--- git temp/Database.h.lab3.bak.h
+++ git temp/Database.h.lab3.bak.h
@@ -7,7 +7,6 @@
 #include "DatalogProgram.h"
 #include "Predicate.h"
 #include <sstream>
-#include <assert.h>
 
 class Database {
 
@@ -27,21 +26,10 @@ public:
 		//
 		// sort the map?  if so, how exactly?
 		// sort the tuples?  if so, how exactly?
+		// 
 	}
 
 	virtual ~Database(){
-	}
-
-	int size(){
-		int count = 0;
-		if (relations.empty()){
-			return 0;
-		}
-		for(map<string,Relation>::iterator i = relations.begin(); i != relations.end(); i++){
-			count += i->second.tuples.size();
-			assert(count >= 0);
-		}
-		return count;
 	}
 
 	map<string,Relation> relations;
@@ -56,17 +44,6 @@ public:
 		}
 		return s;
 	}
-
-	string printRelations(){
-		string s;
-		for (map<string,Relation>::iterator i = relations.begin(); i != relations.end(); i++){
-			s += i->second.Name; // += "\n";
-			s += i->second.toString() += "\n";
-		}
-		return s;
-	}
-
-
 
 	void addRelation(Predicate & src){
 		relations[src.label] = Relation(src);
@@ -85,45 +62,7 @@ public:
 			}
 		}
 	}
-	
-	Relation predPrep(Predicate & q) {
-		Relation src = relations[q.label];
-		// either a copy constructor is needed in
-		// Relation.h or we need to be careful that
-		// q.label points to an extant relation
-		//
-		// somehow a segfault, presumably here,
-		// is killing the execution before even printing 
-		// the DB.
 
-		Relation projected;
-		Relation renamed;
-		selection(src, q);
-		vector<int> cols;
-		for (int i = 0; i < (int) q.params.size(); i++){
-			// if param is var, then add param # to cols
-			// somehow avoid doubles
-		
-			if(q.params[i].ID)
-				cols.push_back(i);
-			for (int k = 0; k < i; k++){
-				if(q.params[i].value == q.params[k].value)
-					cols.pop_back();
-			}	// this is checking if i've already added that var,
-				// then deleting it from end if I have.
-				// "slow" but should work well.
-		}
-		projected = src.project(cols);
-		vector<string> vars;
-		for (int i = 0; i < (int) cols.size(); i++){
-			int index = cols[i];
-			vars.push_back(q.params[index].value);
-		}
-			// figure out how to rename exactly
-		renamed = projected.rename(vars);
-		
-		return renamed;
-	}	
 	
 	string queryEval(Predicate & q) {
 		Relation src = relations[q.label];
